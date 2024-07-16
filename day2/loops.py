11e4cfe2467cbf98846e08f6dd925cc493b2fe4d
Akmargam <aruway333@gmail.com>
2024-07-16 11:56:15 -0400

    indentation

--- day2/loops.py
+++ day2/loops.py
@@ -1,5 +1,11 @@
 #loops
 
-a=1
-for a in range(10,4,-1):#step down loop
-    print(a)
+
+# for a in range(10,4,-1):#step down loop
+#     print(a)
+    
+#sum of first 10 number
+sum=0
+for a in range(1,11):
+    sum=sum+a
+print(sum)
