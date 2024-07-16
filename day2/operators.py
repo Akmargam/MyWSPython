1dea67d23750bbaf17e181b06af12b1746f8c0ce
Akmargam <aruway333@gmail.com>
2024-07-16 11:08:30 -0400

    taking an input

--- day2/operators.py
+++ day2/operators.py
@@ -1,12 +1,18 @@
 # **->exponential
 # //->floor division
 
-print(5**5)# **->exponential
-print(4//3)#//->floor division
+# print(5**5)# **->exponential
+# print(4//3)#//->floor division
 
-#logical operators
+# #logical operators
 
-a=5
-b=4
+# a=5
+# b=4
 
-print(a==b)
+# print(a==b)
+
+
+#taking an input
+print("Please enter a number")
+x=input()
+print("the entered number is: ",x)
