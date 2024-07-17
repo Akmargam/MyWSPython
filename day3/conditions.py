91e7fb32f4382cbb5fb8a401c89cb81d2765d3ec
Akmargam <aruway333@gmail.com>
2024-07-17 11:29:41 -0400

    marks-grade if

--- day3/conditions.py
+++ day3/conditions.py
@@ -7,11 +7,21 @@
 # else:
 #     print("It's a positive number")
 
-a=int(input("Enter a number"))
-if(a%2==0):
-    print("It's an even number")
-elif(a==0):
+# a=int(input("Enter a number"))
+# if(a%2==0):
+#     print("It's an even number")
+# elif(a==0):
 
-    print("it's a zero")
+#     print("it's a zero")
+# else:
+#     print("It's an odd number")
+
+marks=int(input("Enter your marks"))
+if marks>90:
+    print("Your grade is A")
+elif 80<=marks<90:
+    print("Your grade is B")
+elif 60<=marks<80:
+    print("Your grade is C")
 else:
-    print("It's an odd number")
+    print("Your grade is D")
