2b540e6e020a76e18a4bb19c08835ca5ab67ccac
Akmargam <aruway333@gmail.com>
2024-07-17 11:21:07 -0400

    odd-even

--- day3/conditions.py
+++ day3/conditions.py
@@ -1,5 +1,17 @@
+# a=int(input("Enter a number"))
+# if(a<0):
+#     print("It's a negative number")
+# elif(a==0):
+
+#     print("it's a zero")
+# else:
+#     print("It's a positive number")
+
 a=int(input("Enter a number"))
-if(a<0):
-    print("It's a negative number")
+if(a%2==0):
+    print("It's an even number")
+elif(a==0):
+
+    print("it's a zero")
 else:
-    print("It's a positive number")
+    print("It's an odd number")
