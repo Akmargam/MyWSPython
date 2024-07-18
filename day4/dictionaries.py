71e5b97c25baa1651343cdb8c9e89401f757dc90
Akmargam <aruway333@gmail.com>
2024-07-18 12:37:16 -0400

    dictionary

--- day4/dictionaries.py
+++ day4/dictionaries.py
@@ -1,2 +1,5 @@
 car={"Make":"Ford","Model":"Eclipse","Year":"2024"}
-print(type(car))
+print(car["Year"])
+print(len(car))
+car1={"Make":"Ford","Model":["Eclipse","Mustang","Cruzer"],"Year":"2024"}
+print(car1["Model"])
