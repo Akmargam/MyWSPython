f96a1404b8122132f655a7be48b25a7f95319a7e
Akmargam <aruway333@gmail.com>
2024-07-18 11:25:25 -0400

    list + and extend

--- day4/list.py
+++ day4/list.py
@@ -1,6 +1,13 @@
-l1=["apple","kiwi","berry","pear"]
-l2=[]
-for ele in l1:
-    if 'a' not in ele:
-        l2.append(ele)
+# l1=["apple","kiwi","berry","pear"]
+# l2=[]
+# for ele in l1:
+#     if 'a' not in ele:
+#         l2.append(ele)
+# print(l2)
+
+l1=[1,2,3,4,[3,4,6,8,11],34,55]
+l2=[1.1,2.2,3.3,4.4]
+l3=l1+l2
+l2.extend(l1)
 print(l2)
+print(l3)
