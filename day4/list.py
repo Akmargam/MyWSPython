06cf0447d0357235d3fb83bfdffb3bc9b53433ef
Akmargam <aruway333@gmail.com>
2024-07-18 10:48:50 -0400

    list pop

--- day4/list.py
+++ day4/list.py
@@ -1,7 +1,9 @@
 l1=[3,52,5,63,2,6,7,237,7,1]
-l1.append(333)#adding elements
-l1.insert(1,444)
-l1.insert(3,"arun")
-print(l1)
-l1.remove(444)
+# l1.append(333)#adding elements
+# l1.insert(1,444)
+# l1.insert(3,"arun")
+# print(l1)
+# l1.remove(444)
+# print(l1)
+l1.pop()
 print(l1)
