c40236f2d1ed017346e6706d49faa42bdecf05af
Akmargam <aruway333@gmail.com>
2024-07-18 11:00:11 -0400

    list reverse

--- day4/list.py
+++ day4/list.py
@@ -1,10 +1,3 @@
 l1=[3,52,5,63,2,6,7,237,7,1]
-# l1.append(333)#adding elements
-# l1.insert(1,444)
-# l1.insert(3,"arun")
-# print(l1)
-# l1.remove(444)
-# print(l1)
-# l1.pop(1)
-l1.sort()
+l1.reverse()
 print(l1)
