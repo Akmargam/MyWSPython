611be77a2e2bf01248905a14349f4f4948d7309d
Akmargam <aruway333@gmail.com>
2024-07-19 08:28:10 -0400

    dict-keys print

--- day4/dictionaries.py
+++ day4/dictionaries.py
@@ -5,9 +5,8 @@ car1={"Make":"Ford","Model":["Eclipse","Mustang","Cruzer"],"Year":"2024"}
 car1["Mileage"]=100#adding a new key pair
 # print(car1)
 
-for x in car1:#prints keys
+for x in car1.keys():#prints keys
     print(x)
-for x in car1:#prints values
-    print(car1[x])
+
 
 
