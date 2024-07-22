76b4b1d3d0f9bb7b29eb06d4049717a86c02e547
Akmargam <aruway333@gmail.com>
2024-07-22 11:18:25 -0400

    set intersection

--- day5/set.py
+++ day5/set.py
@@ -1,12 +1,15 @@
 #Intro to set
 # no duplicates
-s1={1,2,3,5,6,3}
 # print(type(s1))
 # print(len(s1))
 # list1=[1,2,3,4,5,6,7,8,1,1,12,2,2,3,3,3,4,5,6,7,8,9]
 # set1=set(list1)
 # print(set1)
+s1={1,2,3,5,6,3}
 s2={4,2,6,4,8,9,1,5}
 #union
 print(s1|s2)
 print(s1.union(s2))
+#intersection
+print(s1&s2)
+print(s1.intersection(s2))
