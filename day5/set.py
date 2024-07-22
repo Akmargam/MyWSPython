26449a71bccd9bd471ac7e9665e27dc6d178f396
Akmargam <aruway333@gmail.com>
2024-07-22 11:17:39 -0400

    set union

--- day5/set.py
+++ day5/set.py
@@ -3,6 +3,10 @@
 s1={1,2,3,5,6,3}
 # print(type(s1))
 # print(len(s1))
-list1=[1,2,2,3,4,5,6,3,4]
-set1=set(list1)
-print(set1)
+# list1=[1,2,3,4,5,6,7,8,1,1,12,2,2,3,3,3,4,5,6,7,8,9]
+# set1=set(list1)
+# print(set1)
+s2={4,2,6,4,8,9,1,5}
+#union
+print(s1|s2)
+print(s1.union(s2))
