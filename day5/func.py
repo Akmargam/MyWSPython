0fbd1711f36832b32d7359efe82bd7c5939b7592
Akmargam <aruway333@gmail.com>
2024-07-24 09:00:00 -0400

    func oddEven

--- day5/func.py
+++ day5/func.py
@@ -4,10 +4,18 @@
 
 # greeting("Arun")
 
-def sum(num1,num2):
-    total=num1+num2
-    return total
-num1=int(input("Enter number 1"))
-num2=int(input("Enter number 2"))
-result=sum(num1,num2)
-print(result)
+# def sum(num1,num2):
+#     total=num1+num2
+#     return total
+# num1=int(input("Enter number 1"))
+# num2=int(input("Enter number 2"))
+# result=sum(num1,num2)
+# print(result)
+
+def oddeven(num):
+    if(num%2==0):
+        print("it's even number")
+    else:
+        print("It' Odd")
+num=int(input("Enter a number"))
+oddeven(num)
