9b9579c1fd6bd0cdc8eef93075026d0269604614
Akmargam <aruway333@gmail.com>
2024-07-24 09:30:00 -0400

    file ops

--- day6/fileops.py
+++ day6/fileops.py
@@ -1,3 +1,7 @@
+import json
+
+
 with open("/Users/arunkumarmargam/Desktop/MyWSPython/day6/person.json",'r') as fp:
-    data =fp.read()
+    # data =fp.read()#retuns string
+    data=json.load(fp)#returns dictionary
 print(data)
