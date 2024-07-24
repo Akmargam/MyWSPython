fbb4e7f998ce5889eaabb0d537797e5b6a4ef344
Akmargam <aruway333@gmail.com>
2024-07-24 08:54:38 -0400

    func intro

--- day5/func.py
+++ day5/func.py
@@ -1,5 +1,11 @@
-def greeting(name):
-    print("Hello",name," How are you")
+# def greeting(name):
+#     print("Hello",name," How are you")
     
 
-greeting("Arun")
+# greeting("Arun")
+
+def sum(num1,num2):
+    total=num1+num2
+    return total
+result=sum(9,9)
+print(result)
