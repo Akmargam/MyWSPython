f0dcbca53b2b08436b8c68ffe8e7966f28c2612f
Akmargam <aruway333@gmail.com>
2024-07-24 10:05:31 -0400

    random

--- day7/randomDemo.py
+++ day7/randomDemo.py
@@ -1,3 +1,6 @@
 import random
 l1=[2,45,77,34,67,334,8,99]
-print(random.choice(l1))
+# print(random.choice(l1))
+
+print(random.random())#returns between 0-1
+print(random.randint(2,20))#returns between given numbers
