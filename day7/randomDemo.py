db5a415e59fa4fd7eef29c27eb53737fba7c64be
Akmargam <aruway333@gmail.com>
2024-07-24 10:11:55 -0400

    random-seed

--- day7/randomDemo.py
+++ day7/randomDemo.py
@@ -2,5 +2,14 @@ import random
 l1=[2,45,77,34,67,334,8,99]
 # print(random.choice(l1))
 
-print(random.random())#returns between 0-1
-print(random.randint(2,20))#returns between given numbers
+# print(random.random())#returns between 0-1
+# print(random.randint(2,20))#returns between given numbers
+
+# random.shuffle(l1)
+# print(l1)
+
+random.seed("project2")
+print(random.random())
+random.seed("project1")
+print(random.random())
+
