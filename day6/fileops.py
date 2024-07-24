928fe6c826bc9969fcac31007a69727bb53173fb
Akmargam <aruway333@gmail.com>
2024-07-24 09:35:27 -0400

    file ops-write

--- day6/fileops.py
+++ day6/fileops.py
@@ -1,7 +1,17 @@
 import json
 
 
-with open("/Users/arunkumarmargam/Desktop/MyWSPython/day6/person.json",'r') as fp:
-    # data =fp.read()#retuns string
-    data=json.load(fp)#returns dictionary
-print(data)
+# with open("/Users/arunkumarmargam/Desktop/MyWSPython/day6/person.json",'r') as fp:
+#     # data =fp.read()#retuns string
+#     data=json.load(fp)#returns dictionary
+# print(data)
+
+# write operation
+
+citizen ={
+    "name":"Peter",
+    "Profession":"Hotelier",
+    "Cars":["RAM","VV","MB"]
+}
+with open("Peter.json",'w') as fp:
+    json.dump(citizen,fp)
