d51b97073d295f004251a2d241c1ec41fe23a970
Akmargam <aruway333@gmail.com>
2024-07-25 10:50:17 -0400

    oop-encapsulation

--- day7/oopEx.py
+++ day7/oopEx.py
@@ -1,13 +1,13 @@
 class Book:
-    def __init__(self,author,book,price,discount):#constructor
+    def __init__(self,author,book,price):#constructor
         self.author=author
         self.book=book
         self.price=price
-        self.discount=discount
+        self.__discount=None #__ makes it private(Encapsulation)
         
         
     def __repr__(self):#
-        return f"Author: {self.author}, Book: {self.book}, Price: {self.price}, Dis: {self.discount}"
+        return f"Author: {self.author}, Book: {self.book}, Price: {self.price}"
 
-book1=Book("MK","Truth",200,30)
+book1=Book("MK","Truth",200)
 print(book1)
